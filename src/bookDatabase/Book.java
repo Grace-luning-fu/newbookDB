3df187dec1a4715d25aac4f16172cf892d78d233
grace-luning-fu <grace.luning.fu@gmail.com>
2017-07-29 01:59:19 -0400

    adjust format

--- src/bookDatabase/Book.java
+++ src/bookDatabase/Book.java
@@ -4,76 +4,73 @@ import java.util.ArrayList;
 
 public class Book {
 
-	// Initiate private variable name and price 
-	private String SKU; 
+	// Initiate private variable name and price
+	private String SKU;
 	private String title;
 	private String author;
 	private String description;
 	private String category;
-	private double price; 
-	
-    // method of set and get SKU
-    public void setSku(String SKU){
-    	this.SKU=SKU;
-    }
-    
-    public String getSku(){
-    	return SKU;
-    	
-    }
-    
-    // method of set and get title
-    public void setTitle(String title){
-    	this.title=title;
-    }
-    
-    public String getTitle(){
-    	return title;
-    	
-    }
-    
-    // method of set and get author
-    public void setAuthor(String author){
-    	this.author=author;
-    }
-    
-    public String getAuthor(){
-    	return author;
-    	
-    }
-    
-    
-    // method of set and get description
-    public void setDescription(String description){
-    	this.description = description;
-    }
-    
-    public String getDescription(){
-    	return description;
-    	
-    }
- 
-    // method of set and get category
-    public void setCategory (String category){
-    	this.category = category;
-    }
-    
-    public String getCategory(){
-    	return category;
-    	
-    }
- 
-    // method of set and get price
-    public void setPrice(double price){
-    	this.price=price;
-    	
-    }
-    
-    public double getPrice(){
-    	return price;
-    	
-    }
-    
-}
-    
+	private double price;
 
+	// method of set and get SKU
+	public void setSku(String SKU) {
+		this.SKU = SKU;
+	}
+
+	public String getSku() {
+		return SKU;
+
+	}
+
+	// method of set and get title
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+
+	}
+
+	// method of set and get author
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getAuthor() {
+		return author;
+
+	}
+
+	// method of set and get description
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+
+	}
+
+	// method of set and get category
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return category;
+
+	}
+
+	// method of set and get price
+	public void setPrice(double price) {
+		this.price = price;
+
+	}
+
+	public double getPrice() {
+		return price;
+
+	}
+
+}
--- src/bookDatabase/BookSearchApp.java
+++ src/bookDatabase/BookSearchApp.java
@@ -3,16 +3,15 @@ package bookDatabase;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class BookSearchApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		/**
-		 * This is a program to store information of books into a database. 
-		 * It also allows you to search a book by its SKU, and display all information about this book
+		 * This is a program to store information of books into a database. It also
+		 * allows you to search a book by its SKU, and display all information about
+		 * this book
 		 * 
 		 * @author Grace
 		 **/
@@ -21,212 +20,207 @@ public class BookSearchApp {
 		String exist = null;
 		String existA = null;
 		int choice = 0;
-		
+
 		// adding the 6 books to the database
-		Book Book1= new Book();
+		Book Book1 = new Book();
 		Book1.setSku("Java1001");
 		Book1.setTitle("Head First Java");
 		Book1.setAuthor("Kathy Sierra and Bert Bates");
 		Book1.setDescription("Easy to read Java workbook");
 		Book1.setPrice(47.50);
-		
-		
-		Book Book2= new Book();
+
+		Book Book2 = new Book();
 		Book2.setSku("Java1002");
 		Book2.setTitle("Thinking in Java");
 		Book2.setAuthor("Bruce Eckel");
 		Book2.setDescription("Details about Java under the hood");
 		Book2.setPrice(20.00);
-		
-		
-		Book Book3= new Book();
+
+		Book Book3 = new Book();
 		Book3.setSku("Orcl1003");
 		Book3.setTitle("OCP: Oracle Certified Professional Java SE");
 		Book3.setAuthor("Jeanne Boyarsky");
 		Book3.setDescription("Everything you need to know in one place");
 		Book3.setPrice(45.00);
-		
-		Book Book4= new Book();
+
+		Book Book4 = new Book();
 		Book4.setSku("Python1004");
 		Book4.setTitle("Automate the Boring Stuff with Python");
 		Book4.setAuthor("Al Sweigart");
 		Book4.setDescription("Fun with Python");
 		Book4.setPrice(10.50);
-		
-		Book Book5= new Book();
+
+		Book Book5 = new Book();
 		Book5.setSku("Zombie1005");
 		Book5.setTitle("The Maker's Guide to the Zombie Apocalypse");
 		Book5.setAuthor("Simon Monk");
 		Book5.setDescription("Defend Your Base with Simple Circuits, Arduino, and Raspberry Pi");
 		Book5.setPrice(16.50);
-		
-		Book Book6= new Book();
+
+		Book Book6 = new Book();
 		Book6.setSku("Rasp1006");
 		Book6.setTitle("Raspberry Pi Projects for the Evil Genius");
 		Book6.setAuthor("Donald Norris");
 		Book6.setDescription("A dozen fiendishly fun projects for the Raspberry Pi!");
 		Book6.setPrice(14.75);
-		
+
 		arr.add(Book1);
 		arr.add(Book2);
 		arr.add(Book3);
 		arr.add(Book4);
 		arr.add(Book5);
 		arr.add(Book6);
-		
+
 		// continue when the user does not choose to quit
 		do {
-		//display the menu
-		System.out.println("\n\n===Menu===");
-		System.out.println("1.Display the all books in database");
-		System.out.println("2.Add a new entry to the database");
-		System.out.println("3.Search a book by SKU");
-		System.out.println("4.Search a book by Author/co-Author");
-		System.out.println("5.Quit");
-		
-		System.out.println("Please choose from option 1 to 5:");
-		Scanner scop = new Scanner(System.in); 
-		choice=scop.nextInt();
+			// display the menu
+			System.out.println("\n\n===Menu===");
+			System.out.println("1.Display the all books in database");
+			System.out.println("2.Add a new entry to the database");
+			System.out.println("3.Search a book by SKU");
+			System.out.println("4.Search a book by Author/co-Author");
+			System.out.println("5.Quit");
 
-		// To display all book in the database
-	    if (choice==1) {
-	    	System.out.println("");
-	    	System.out.println("----------------------------------------------------------------------------------------"+
-	    			"---------------------------------------------------------------------------------------------------");
-	    	System.out.printf("\n"+"%6s%26s%40s%36s%52s","SKU", "Title", "Author", "Description", "Price");
-	    	System.out.println("\n--------------------------------------------------------------------------------------"+
-	    			"---------------------------------------------------------------------------------------------------");
-		//Enhanced loop over the ArrayList arr and display the book database
-		for (Book item: arr) {
-				System.out.printf("\n"+"%-12s%-45s%-30s%-69s%-10s", item.getSku(), item.getTitle(), 
-						item.getAuthor(), item.getDescription(),
-						item.getPrice());
-			}	   	
-	    }
-		
-	    // Allow the user to create a book entry, and add all information of the book
-	    if (choice==2) {
-		
-	    	do{
-			  
-        // Create a new instance of Product as an item in ArrayList
-		Book onebook = new Book();
-        
-		//Prompt user input for SKU
-		System.out.println("Input a SKU of the book:");
-		Scanner scSKU = new Scanner(System.in);
-			
-		String SKU = scSKU.nextLine();
-		onebook.setSku(SKU);
-		
-		//Prompt user input for title
-		System.out.println("Input a Title of the book:");
-		Scanner sctitle = new Scanner(System.in);
-			
-		String title = sctitle.nextLine();
-		onebook.setTitle(title);
-		
-		//Prompt user input for an author
-		System.out.println("Input the author of the book:");
-		Scanner scauthor = new Scanner(System.in);
-			
-		String author = scauthor.nextLine();
-		onebook.setSku(author);
+			System.out.println("Please choose from option 1 to 5:");
+			Scanner scop = new Scanner(System.in);
+			choice = scop.nextInt();
 
-		//Prompt user input for description
-		System.out.println("Input the description of the book:");
-		Scanner scde = new Scanner(System.in);
-			
-		String des = scde.nextLine();
-		onebook.setDescription(des);
-		
-		//Prompt user input for price
-		System.out.println("Input the price of the book:");
-		Scanner scpri = new Scanner(System.in);
-		
-		Double price = scpri.nextDouble();
-		onebook.setPrice(price);
-	    
-		// prompt for choice of continue or not 
-		System.out.println("Would you like to enter another book? y/n");
-		Scanner scan00 = new Scanner(System.in);
-		contin= scan00.nextLine(); 
-		
-		//add this item in to the ArrayList
-		arr.add(onebook);
-		
-		} while (contin.equalsIgnoreCase("y"));
-        
-	    }
-		
-		
-	    //look up a book by SKU, loop through the ArrayList, if no match, print the book is not in the database
-	    if (choice ==3) {
-		System.out.println("Please enter the book SKU for searching.");
-		Scanner scan01 = new Scanner(System.in);
-		String searchSKU= scan01.nextLine(); 
-		
-		int counterS =0; 
-		
-		while (exist!="yes"&& counterS<arr.size()){
-			
-			exist=null;
-			
-			if (searchSKU.equalsIgnoreCase(arr.get(counterS).getSku())){
-				System.out.println("SKU: " + arr.get(counterS).getSku() + "    " + "Title: " + arr.get(counterS).getTitle() + "    "+ 
-					     "Author: " + arr.get(counterS).getAuthor() + "    " + "Description" + arr.get(counterS).getDescription() +
-						 "    " + "Price: $" + arr.get(counterS).getPrice());
-				
-				exist = "yes";
+			// To display all book in the database
+			if (choice == 1) {
+				System.out.println("");
+				System.out.println(
+						"----------------------------------------------------------------------------------------"
+								+ "---------------------------------------------------------------------------------------------------");
+				System.out.printf("\n" + "%6s%26s%40s%36s%52s", "SKU", "Title", "Author", "Description", "Price");
+				System.out.println(
+						"\n--------------------------------------------------------------------------------------"
+								+ "---------------------------------------------------------------------------------------------------");
+				// Enhanced loop over the ArrayList arr and display the book database
+				for (Book item : arr) {
+					System.out.printf("\n" + "%-12s%-45s%-30s%-69s%-10s", item.getSku(), item.getTitle(),
+							item.getAuthor(), item.getDescription(), item.getPrice());
+				}
 			}
-			counterS ++; 
-		}
 
-			
-		if (exist!="yes"){
-			System.out.println("The book with this SKU is not in the database.");
-		}
-		
-	    }
-	    
-	    
-	    
-	    //look up a book by SKU, loop through the Author, if no match, print the book is not in the database
-	    if (choice==4) {
-			
-		System.out.println("Please enter the author for searching.");
-		Scanner scan02 = new Scanner(System.in);
-		String searchAuthor= scan02.nextLine(); 
-		
-		int counterA =0; 
-		
-		while ( existA!="yes"&& counterA<arr.size()){
-			
-			existA=null;
-			
-			if (arr.get(counterA).getAuthor().contains(searchAuthor)){
-				System.out.println( "SKU: " + arr.get(counterA).getSku() + "    " + "Title: " + arr.get(counterA).getTitle() + "    "+ 
-					     "Author: " + arr.get(counterA).getAuthor() + "    " + "Description" + arr.get(counterA).getDescription() +
-						 "    " + "Price: $" + arr.get(counterA).getPrice());
-				
-				existA = "yes";
+			// Allow the user to create a book entry, and add all information of the book
+			if (choice == 2) {
+
+				do {
+
+					// Create a new instance of Product as an item in ArrayList
+					Book onebook = new Book();
+
+					// Prompt user input for SKU
+					System.out.println("Input a SKU of the book:");
+					Scanner scSKU = new Scanner(System.in);
+
+					String SKU = scSKU.nextLine();
+					onebook.setSku(SKU);
+
+					// Prompt user input for title
+					System.out.println("Input a Title of the book:");
+					Scanner sctitle = new Scanner(System.in);
+
+					String title = sctitle.nextLine();
+					onebook.setTitle(title);
+
+					// Prompt user input for an author
+					System.out.println("Input the author of the book:");
+					Scanner scauthor = new Scanner(System.in);
+
+					String author = scauthor.nextLine();
+					onebook.setSku(author);
+
+					// Prompt user input for description
+					System.out.println("Input the description of the book:");
+					Scanner scde = new Scanner(System.in);
+
+					String des = scde.nextLine();
+					onebook.setDescription(des);
+
+					// Prompt user input for price
+					System.out.println("Input the price of the book:");
+					Scanner scpri = new Scanner(System.in);
+
+					Double price = scpri.nextDouble();
+					onebook.setPrice(price);
+
+					// prompt for choice of continue or not
+					System.out.println("Would you like to enter another book? y/n");
+					Scanner scan00 = new Scanner(System.in);
+					contin = scan00.nextLine();
+
+					// add this item in to the ArrayList
+					arr.add(onebook);
+
+				} while (contin.equalsIgnoreCase("y"));
+
 			}
-			counterA ++; 
-		}
-			
-		if (existA!="yes"){
-			System.out.println("The book is not in the data base.");
-		}
-			
-		} 
-	    
-		}while(choice!=5);
-	    
 
-	    	   System.out.println("Thank you for using this App");
-	    
+			// look up a book by SKU, loop through the ArrayList, if no match, print the
+			// book is not in the database
+			if (choice == 3) {
+				System.out.println("Please enter the book SKU for searching.");
+				Scanner scan01 = new Scanner(System.in);
+				String searchSKU = scan01.nextLine();
+
+				int counterS = 0;
+
+				while (exist != "yes" && counterS < arr.size()) {
+
+					exist = null;
+
+					if (searchSKU.equalsIgnoreCase(arr.get(counterS).getSku())) {
+						System.out.println("SKU: " + arr.get(counterS).getSku() + "    " + "Title: "
+								+ arr.get(counterS).getTitle() + "    " + "Author: " + arr.get(counterS).getAuthor()
+								+ "    " + "Description" + arr.get(counterS).getDescription() + "    " + "Price: $"
+								+ arr.get(counterS).getPrice());
+
+						exist = "yes";
+					}
+					counterS++;
+				}
+
+				if (exist != "yes") {
+					System.out.println("The book with this SKU is not in the database.");
+				}
+
+			}
+
+			// look up a book by SKU, loop through the Author, if no match, print the book
+			// is not in the database
+			if (choice == 4) {
+
+				System.out.println("Please enter the author for searching.");
+				Scanner scan02 = new Scanner(System.in);
+				String searchAuthor = scan02.nextLine();
+
+				int counterA = 0;
+
+				while (existA != "yes" && counterA < arr.size()) {
+
+					existA = null;
+
+					if (arr.get(counterA).getAuthor().contains(searchAuthor)) {
+						System.out.println("SKU: " + arr.get(counterA).getSku() + "    " + "Title: "
+								+ arr.get(counterA).getTitle() + "    " + "Author: " + arr.get(counterA).getAuthor()
+								+ "    " + "Description" + arr.get(counterA).getDescription() + "    " + "Price: $"
+								+ arr.get(counterA).getPrice());
+
+						existA = "yes";
+					}
+					counterA++;
+				}
+
+				if (existA != "yes") {
+					System.out.println("The book is not in the data base.");
+				}
+
+			}
+
+		} while (choice != 5);
+
+		System.out.println("Thank you for using this App");
+
 	}
 }
-	
-	
-
